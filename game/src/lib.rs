aefadb1799b074d0a99b2db3fbd94ef3fd1c8ac4
dougfort <doug.fort@gmail.com>
2018-07-20 15:14:08 -0400

    put suffled cards in the reserve

--- game/src/lib.rs
+++ game/src/lib.rs
@@ -1,17 +1,34 @@
 // game definitions
 
+extern crate rand;
 extern crate cards;
+
+use rand::{Rng, thread_rng};
+
+#[derive(Debug, PartialOrd, PartialEq, Clone)]
+pub struct Column {
+    pub cards_in_play: Vec<cards::Card>,
+    pub visible_count: u32,
+}
 
 #[derive(Debug)]
 pub struct Game {
     pub reserve: Vec<cards::Card>,
+    pub layout: Vec<Column>,
 }
 
 impl Game {
     pub fn new() -> Game {
-        let deck = cards::deck();
-
-        Game { reserve: deck }
+        let mut reserve = [cards::deck(), cards::deck()].concat();
+        thread_rng().shuffle(&mut reserve);
+        
+        let empty_cards: Vec<cards::Card> = vec![];
+        let empty_column = Column{cards_in_play: empty_cards, visible_count: 0};
+    
+        Game { 
+            reserve: reserve,
+            layout: vec![empty_column],
+        }
     }
 }
 
