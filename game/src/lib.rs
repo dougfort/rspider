ed3e87f6e121703bdaedc29177668ca54b60f40c
dougfort <doug.fort@gmail.com>
2018-07-21 15:49:24 -0400

    create the full initial game

--- game/src/lib.rs
+++ game/src/lib.rs
@@ -22,20 +22,34 @@ impl Game {
         let mut reserve = [cards::deck(), cards::deck()].concat();
         thread_rng().shuffle(&mut reserve);
         
-        let empty_cards: Vec<cards::Card> = vec![];
-        let empty_column = Column{cards_in_play: empty_cards, visible_count: 0};
+        let layout = [6, 5, 5, 6, 5, 5, 6, 5, 5, 6].iter().map(|n| {            
+            Column{
+                cards_in_play: reserve.drain(..n).collect(),
+                visible_count: 1,
+            }
+        }).collect();        
     
         Game { 
             reserve: reserve,
-            layout: vec![empty_column],
+            layout: layout,
         }
     }
 }
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn layout_size() {
+        let x_size: Vec<usize> = vec![6, 5, 5, 6, 5, 5, 6, 5, 5, 6];
+        let x_layout_size: usize = x_size.iter().sum();
+        let deck_size: usize = 2 * 52;
+        let g = Game::new();
+        for (l, a) in g.layout.iter().zip(x_size.iter()) {
+            assert_eq!(l.visible_count, 1);
+            assert_eq!(l.cards_in_play.len(), *a);
+        }
+        assert_eq!(g.reserve.len(), deck_size - x_layout_size);
     }
 }
