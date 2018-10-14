079366b500819276bc85c0923f8b270d76fc217e
dougfort <doug.fort@gmail.com>
2018-10-14 18:00:54 -0400

    crude display of game (#1)

--- player/src/main.rs
+++ player/src/main.rs
@@ -3,7 +3,18 @@ extern crate game;
 
 fn main() {
     let game = game::Game::new();
-    for card in game.reserve {
-        println!("card = {}", card);
-    }
+    let max_col = game.layout.iter().map(|col| {
+        col.cards_in_play.len()
+    }).max().unwrap();
+    for y in 0..max_col {
+        let result = game.layout.iter().fold("".to_string(), |line, col| {
+            let entry = match col.cards_in_play.len() {
+                len if len > y &&  y >= len - col.visible_count => format!("{}", col.cards_in_play[y]),
+                len if len > y  => format!("(----)"),
+                _ => format!{"          "}
+            };
+            format!("{} {:<10}", line, entry)
+        });
+        println!("{}", result);
+    };
 }
