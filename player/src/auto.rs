878ca1f05404dff16198c3990f880966b6c1a946
dougfort <doug.fort@gmail.com>
2019-01-10 09:28:48 -0500

    implement undo for move

--- player/src/auto.rs
+++ player/src/auto.rs
@@ -4,13 +4,20 @@ use std::error::Error;
 pub fn play(client: &mut client::Client) -> Result<(), Box<Error>> {
     let mut play = 0;
 
-    loop {
+    'play_loop: loop {
         play += 1;
         println!("play: {}", play);
 
         let moves = client.possible_moves()?;
         if moves.is_empty() {
-
+            if client.cards_dealt() == client.total_cards() {
+                println!("cards exhausted");
+                break 'play_loop;
+            } else {
+                client.deal()?;
+            }
+        } else {
+            client.move_cards(moves[1])?;
         }
     }
 
