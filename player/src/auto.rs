003e03ad4de8357736e74499da86561b1a04b416
dougfort <doug.fort@gmail.com>
2019-01-10 15:01:04 -0500

    WIP on auto

--- player/src/auto.rs
+++ player/src/auto.rs
@@ -1,8 +1,11 @@
 use std::error::Error;
+use std::{thread, time};
 
+use display;
 
 pub fn play(client: &mut client::Client) -> Result<(), Box<Error>> {
     let mut play = 0;
+    let mut prev_move = (0, 0, 0);
 
     'play_loop: loop {
         play += 1;
@@ -14,11 +17,25 @@ pub fn play(client: &mut client::Client) -> Result<(), Box<Error>> {
                 println!("cards exhausted");
                 break 'play_loop;
             } else {
+                println!(">>> dealing");
                 client.deal()?;
             }
         } else {
-            client.move_cards(moves[1])?;
+            let action = moves[1];            
+            println!(">>> {:?}", action);
+            let current_move = (action.orig_col, action.count, action.dest_col);
+            if current_move == prev_move {
+                println!("*** duplicate move");
+            };
+            client.move_cards(action)?;
+            prev_move = current_move;
         }
+
+        display::local_game(&client);
+        display::possible_moves(&client)?;
+
+        let wait_time = time::Duration::from_secs(2);
+        thread::sleep(wait_time);
     }
 
     Ok(())
