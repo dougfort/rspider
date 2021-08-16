61fa4378e5f928c7fc3f6bcc564db1ee9e69f6f5
dougfort <doug.fort@gmail.com>
2021-08-16 18:32:48 -0400

    use rustyline

--- player/src/main.rs
+++ player/src/main.rs
@@ -1,5 +1,6 @@
 use anyhow::Result;
-use std::io::{stdin, stdout, Write};
+use rustyline::error::ReadlineError;
+use rustyline::Editor;
 
 mod auto;
 mod display;
@@ -9,76 +10,88 @@ fn main() -> Result<()> {
         Some(seed) => client::Client::from_hex(&seed)?,
         None => client::Client::new()?,
     };
+
+    let mut rl = Editor::<()>::new();
+    if rl.load_history("history.txt").is_err() {
+        println!("No previous history.");
+    }
+
     loop {
         display::local_game(&client);
         display::possible_moves(&client)?;
 
-        let stdin_line = get_stdin_line(">")?;
-        let command: Vec<&str> = stdin_line.trim().split_whitespace().collect();
-        if command.is_empty() {
-            continue;
-        }
-        match command[0].trim() {
-            "" => continue,
-            "help" => display::help(),
-            "quit" => break,
-            "deal" => {
-                if client.cards_dealt() < client.total_cards() {
-                    client.deal()?;
-                };
-            }
-            "move" => {
-                if command.len() < 2 {
-                    println!("you must specify the number of a move");
+        let readline = rl.readline(">> ");
+        match readline {
+            Ok(line) => {
+                rl.add_history_entry(line.as_str());
+                let command: Vec<&str> = line.trim().split_whitespace().collect();
+                if command.is_empty() {
                     continue;
                 }
-                match command[1].parse::<usize>() {
-                    Ok(n) => {
-                        let moves = client.possible_moves()?;
-                        if n > moves.len() {
-                            println!("move number {} out of bounds", n);
+                match command[0].trim() {
+                    "" => continue,
+                    "help" => display::help(),
+                    "quit" => break,
+                    "deal" => {
+                        if client.cards_dealt() < client.total_cards() {
+                            client.deal()?;
+                        };
+                    }
+                    "move" => {
+                        if command.len() < 2 {
+                            println!("you must specify the number of a move");
                             continue;
                         }
-                        client.move_cards(moves[n - 1].mv)?;
+                        match command[1].parse::<usize>() {
+                            Ok(n) => {
+                                let moves = client.possible_moves()?;
+                                if n > moves.len() {
+                                    println!("move number {} out of bounds", n);
+                                    continue;
+                                }
+                                client.move_cards(moves[n - 1].mv)?;
+                            }
+                            Err(e) => {
+                                println!("invalid move number {}", e);
+                                continue;
+                            }
+                        };
                     }
-                    Err(e) => {
-                        println!("invalid move number {}", e);
+                    "checkpoints" => {
+                        for cp in client.checkpoints() {
+                            println!("{:?}", cp);
+                        }
+                    }
+                    "undo" => {
+                        if client.checkpoints().len() < 2 {
+                            println!("nothing to undo");
+                            continue;
+                        };
+                        client.undo()?;
+                    }
+                    "auto" => {
+                        auto::play(&mut client)?;
+                    }
+                    _ => {
+                        println!("invalid input");
                         continue;
                     }
-                };
-            }
-            "checkpoints" => {
-                for cp in client.checkpoints() {
-                    println!("{:?}", cp);
                 }
             }
-            "undo" => {
-                if client.checkpoints().len() < 2 {
-                    println!("nothing to undo");
-                    continue;
-                };
-                client.undo()?;
+            Err(ReadlineError::Interrupted) => {
+                println!("CTRL-C");
+                break;
             }
-            "auto" => {
-                auto::play(&mut client)?;
+            Err(ReadlineError::Eof) => {
+                println!("CTRL-D");
+                break;
             }
-            _ => {
-                println!("invalid input");
-                continue;
+            Err(err) => {
+                println!("Error: {:?}", err);
+                break;
             }
         }
     }
 
     Ok(())
-}
-
-fn get_stdin_line(prompt: &str) -> std::io::Result<String> {
-    println!();
-    print!("{} ", prompt);
-    stdout().flush()?;
-
-    let mut input = String::new();
-    stdin().read_line(&mut input)?;
-
-    Ok(input)
 }
